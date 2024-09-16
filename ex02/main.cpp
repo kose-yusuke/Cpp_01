802bf6d529fbe395b194c5c4a326befd88a57d02
kose-yusuke <yusuke188alc@icloud.com>
2024-09-16 11:05:19 +0900

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: koseki.yusuke <koseki.yusuke@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 10:53:15 by koseki.yusu       #+#    #+#             */
+/*   Updated: 2024/09/16 10:57:17 by koseki.yusu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string brain = "HI THIS IS BRAIN";
+    std::string* stringPTR = &brain;
+    std::string& stringREF = brain;
+
+    std::cout << "Memory address of brain: " << &brain << std::endl;
+    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+    std::cout << "Value of brain: " << brain << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+
+    return 0;
+}
+
+
