21de669e61bb4055a8a97e34fc7c1b76145e05e8
kose-yusuke <yusuke188alc@icloud.com>
2024-10-23 21:01:21 +0900

    tmp

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -6,16 +6,14 @@
 /*   By: koseki.yusuke <koseki.yusuke@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 11:18:07 by koseki.yusu       #+#    #+#             */
-/*   Updated: 2024/09/16 11:21:43 by koseki.yusu      ###   ########.fr       */
+/*   Updated: 2024/10/23 20:59:04 by koseki.yusu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-// コンストラクタの定義
 HumanA::HumanA(std::string name, Weapon& weapon) : name(name), weapon(weapon) {}
 
-// attack メソッドの定義
 void HumanA::attack() {
     std::cout << name << " attacks with their " << weapon.getType() << std::endl;
 }
--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,21 +6,18 @@
 /*   By: koseki.yusuke <koseki.yusuke@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 11:18:14 by koseki.yusu       #+#    #+#             */
-/*   Updated: 2024/09/16 11:27:29 by koseki.yusu      ###   ########.fr       */
+/*   Updated: 2024/10/23 20:59:11 by koseki.yusu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-// コンストラクタの定義
 HumanB::HumanB(std::string name) : name(name), weapon(nullptr) {}
 
-// setWeapon メソッドの定義
 void HumanB::setWeapon(Weapon& newWeapon) {
     weapon = &newWeapon;
 }
 
-// attack メソッドの定義
 void HumanB::attack() {
     if (weapon) {
         std::cout << name << " attacks with their " << weapon->getType() << std::endl;
